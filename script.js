4c1ca073af6efbc178d4370f758aeb27f6cf0f7a
Mohammad Safi Maz <142226851+safi-is-coding@users.noreply.github.com>
2024-05-26 15:45:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,22 +3,27 @@
 
 
 function changebg(){
-    if (document.querySelector('body').style.background === 'black'){
+    console.log(document.querySelector('body').style.background);
+
+    if (document.querySelector('body').style.background === 'rgb(27, 27, 27)'){
     document.querySelector('body').style.background = 'white';
     document.querySelector('body').style.color = 'black';
     document.querySelector('.navigation').style.background = 'whitesmoke';
     document.querySelector('.search').style.background = '#f3f3f3';
     document.querySelector('footer').style.background = '#cccccc';
+    document.querySelector('.left1').style.background = 'linear-gradient(190deg, rgba(174, 174, 174, 0.593), whitesmoke)';
 
     }
     else{
-        document.querySelector('body').style.background = 'black';
+        document.querySelector('body').style.background = 'rgb(27, 27, 27)';
         document.querySelector('body').style.color = 'white';
-        document.querySelector('.navigation').style.background = 'darkgray';
+        document.querySelector('.navigation').style.background = '#363636';
+        // document.querySelector('.links a').style.color = 'white';
+        let navbuttons = document.querySelector('.links a');
         document.querySelector('.search').style.background = 'gray';
         document.querySelector('footer').style.background = 'darkgray';
+        document.querySelector('.left1').style.background = 'linear-gradient(180deg, rgba(174, 174, 174, 0.389), #1a1a1a)';
         
-
         
     }
 }
